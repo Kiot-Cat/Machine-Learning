9e77015ede3a28ee9e52ef76330a3e7ec9ee0074
Kiot-Cat <deotc1170@gmail.com>
2026-05-11 20:07:22 -0600

    Act 1 Programa prediccion.py
    Actividad 1, uso de condicionales para ver si se aprueba o reprueba victima sjakjs

---
+++ Act 1 Programa prediccion.py
@@ -0,0 +1,8 @@
+
+calificaion = int(input("Escribe calificacion xddd: ")) #No pues, definimos una variable para despues su tipo de dato y la capturacion de este
+
+#Aqui nos movemos a las condiciones sobre si pasa o no la victima
+if calificaion <= 6:
+    print("NO PASASTE :(") #Se imprime que no pasa
+else:
+    print("NICEE PASASTE :)") #Se imprime que si pasa
